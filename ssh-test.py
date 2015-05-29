6663f2b3e74b63bbad93c5e5fbef9942da4932fc
kingwiiwii <richardrafferty1984@gmail.com>
2015-05-30 00:00:12 +0100

    SSH portion now generic and prompting for IP

--- ssh-test.py
+++ ssh-test.py
@@ -8,18 +8,13 @@ print "2.SSHv2"
 print "\r"
 print "\r"
 
-conn = SSH2() 
+Host_IP = raw_input('Device IP :')
 
-print "Login Account"
-print "============="
-print "1. LSG Login"
-print "2. Thermo Login"
-print "\r"
-print "\r"
+conn = SSH2() 
 
 account = read_login()              
                       
-conn.connect('10.71.31.36')     
+conn.connect(Host_IP)     
 conn.login(account)                 
 
 conn.execute('term len 0')
@@ -33,13 +28,3 @@ f.close()
 conn.send('exit\r')               
 conn.close()    
 
-#from Exscript.util.interact import read_login
-#from Exscript.protocols import SSH2
-#
-#account = read_login()     # Prompt the user for his name and password
-#conn = SSH2()              # We choose to use SSH2
-#conn.connect('localhost')  # Open the SSH connection
-#conn.login(account)        # Authenticate on the remote host
-#conn.execute('uname -a')   # Execute the "uname -a" command
-#conn.send('exit\r')        # Send the "exit" command
-#conn.close()               # Wait for the connection to close
